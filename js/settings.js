b6b926f18ac30e76d9a93d1e083062a8008aa052
Jamário Batista do Nascimento <jamario.nascimento@crp.com>
2021-12-16 17:29:55 -0300

    add template video-vimeo tracking xApi

---
+++ js/settings.js
@@ -0,0 +1,17 @@
+// LRS Connection
+let scormReporting = false;
+let xAPIReporting = true;
+let kerRef = "193cfb8a1c1b220b3006f3ec0ab3c7c3af239fa3";
+let secretRef = "7f9ed13fa42460ce7a227fe80c012880ea2e1d32";
+let endPointRef = "https://localhost/data/xAPI/"
+
+//Reporting Variables
+let objectID = "https://www.learningdojo.net/xapi/course1";
+let objectName = "Video Vímemo";
+
+// Display labels
+let videoID = "464849280";
+let title = 'Exemplo de video vimeo';
+let description = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.";
+let copyrightText = '&#169; 2020 Learning Dojo';
+let profilePic = 'media/me.jpg';
